9e5122e7d1b02d8861e7db3fa32c6ac6a44ce9e8
okaputra <54925385+okaputra@users.noreply.github.com>
2024-01-28 21:55:04 +0800

    implement logo

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -31,7 +31,7 @@
 <!-- Navbar Start -->
 <nav class="navbar navbar-expand-lg bg-white navbar-light sticky-top p-0">
     <a href="/" class="navbar-brand d-flex align-items-center border-end px-4 px-lg-5">
-        <h2 class="m-0"><i class="fa fa-globe text-primary me-2"></i>24English</h2>
+       <img class="img-fluid logo24" src="{{asset('logo/logo.png')}}" alt="">
     </a>
     <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
         <span class="navbar-toggler-icon"></span>
