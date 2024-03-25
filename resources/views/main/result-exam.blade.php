aa403ac7313f93ccdc7eef6ca681d1101b9efadb
davidandreas24 <54925385+okaputra@users.noreply.github.com>
2024-03-25 10:05:30 +0800

    add: views start exam and result exam

---
+++ resources/views/main/result-exam.blade.php
@@ -0,0 +1,122 @@
+@extends('layout.master-main')
+@section('content')
+
+<style>
+    .justify-between{
+        margin-bottom: 20px;
+    }
+</style>
+
+<!-- Courses Start -->
+<div class="container-xxl py-6" style="padding: 4px">
+    <div class="container">
+        <div class="row g-5">
+            <div class="col-lg-8 wow">
+                <h1 class="display-6 mb-4">RESULT: FINAL EXAM</h1>
+                <div class="accordion col-lg-10 wow" id="accordionPanelsStayOpenExample">
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="panelsStayOpen-headingOne">
+                            <button class="accordion-button" type="button" data-bs-toggle="collaps" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
+                                RESULT
+                            </button>
+                        </h2>
+                        <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
+                            <div class="accordion-body">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <p class="card-text">Question &emsp;&emsp;&emsp;&emsp;&nbsp;: <i class="bi bi-book"></i> {{$jumlah_soal}}</p>
+                                        <p class="card-text">Duration &emsp;&emsp;&emsp;&emsp;&nbsp;&nbsp;: <i class="bi bi-clock"></i> {{$exam['durasi']}} Minutes</p>
+                                        <p class="card-text" style="color: green">CORRECT &emsp;&emsp;&emsp;&emsp; : <i class="bi bi-check"></i> {{$correctAnswer}}</p>
+                                        <p class="card-text" style="color: red">INCORRECT &emsp;&emsp;&nbsp;&nbsp;&nbsp;&nbsp;: <i class="bi bi-exclamation-circle"></i> {{$wrongAnswer}}</p>
+                                        <p class="card-text" style="color: red">BLANK &emsp;&emsp;&emsp;&emsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: <i class="bi bi-circle"></i> {{$blankAnswerCount}}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <form method="POST" action="#" id="formJ">
+                    @csrf
+                    @php
+                        $no=($soal->currentpage()-1)* $soal->perpage()+1;
+                    @endphp
+                    @foreach ($soal as $index => $s)
+                    <br>
+                    <div class="form-group">
+                      <div class="accordion-item">
+                            <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
+                                <div class="accordion-body">
+                                    <nav aria-label="breadcrumb">
+                                        <ol class="breadcrumb">
+                                            @if ($isShowClue[$s['id']])
+                                                <li class="breadcrumb-item active" aria-current="page" style="color: red; font-size:2em"><b>{{$no++}}</b></li>
+                                            @else
+                                                <li class="breadcrumb-item active" aria-current="page" style="font-size:2em;"><b>{{$no++}}</b></li>
+                                            @endif
+                                        </ol>
+                                    </nav>
+                                    <div class="card">
+                                        <div class="card-body">
+                                            <p class="card-text"> 
+                                                {!! $s['pertanyaan'] !!}
+                                                @if($s['audio_file'])
+                                                    <audio controls preload="none">
+                                                        <source src="{{ asset('audio-soal/' . $s['audio_file'] . '/' . $s['audio_file'])}}" type="audio/{{ pathinfo($s['audio_file'], PATHINFO_EXTENSION) }}">
+                                                        Your browser does not support the audio element.
+                                                    </audio>
+                                                @endif
+                                            </p>
+                                            @if($s['tipe'] === 'opsi')
+                                                @foreach ($s->opsi as $op)
+                                                    <p class="card-text">
+                                                        <div class="form-check">
+                                                            <input class="form-check-input select_ans" type="radio" name="user_answers[{{$s['id']}}][{{$index+1}}]" id="exampleRadios{{$s['id']}}_{{$index+1}}" value="{{$op['id']}}" @if(in_array($op['id'], $user_answers)) checked @endif disabled>
+                                                            <label class="form-check-label" for="exampleRadios{{$index+1}}">
+                                                                {!! $op['opsi'] !!}
+                                                                @if($op['audio_file'])
+                                                                    <audio controls preload="none">
+                                                                        <source src="{{ asset('audio-opsi/' . $op['audio_file'] . '/' . $op['audio_file'])}}" type="audio/{{ pathinfo($op['audio_file'], PATHINFO_EXTENSION) }}">
+                                                                        Your browser does not support the audio element.
+                                                                    </audio>
+                                                                @endif
+                                                            </label>
+                                                        </div>
+                                                    </p>
+                                                @endforeach
+                                            @else
+                                            @php
+                                                // Find the user answer for this question
+                                                $userAnswerDesc = $user_answers_desc->where('id_question', $s['id'])->first();
+                                            @endphp
+                                            <div class="input-group">
+                                                <textarea rows="7" class="form-control answer_description" name="user_answers[{{$s['id']}}][{{$index+1}}]" aria-label="With textarea" disabled>{{$userAnswerDesc ? $userAnswerDesc['user_answer'] : ''}} </textarea>
+                                            </div>
+                                            @endif
+                                        </div>
+                                    </div>
+                                    @if ($isShowClue[$s['id']])
+                                        <br>
+                                        <div class="alert alert-warning" role="alert">
+                                            <b>Clue: {{$s['clue']}}</b>
+                                        </div>
+                                    @endif
+                                    @if ($blankAnswer[$s['id']])
+                                        <br>
+                                        <div class="alert alert-danger" role="alert">
+                                            <b>Not Answered!</b>
+                                        </div>
+                                    @endif
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    @endforeach
+                </form>
+                <br>
+                {{ $soal->links() }}
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Courses End -->
+@endsection
---
+++ resources/views/main/start-exam.blade.php
@@ -0,0 +1,222 @@
+@extends('layout.master-main')
+@section('content')
+
+<style>
+    .justify-between{
+        margin-bottom: 20px;
+    }
+</style>
+
+<!-- Courses Start -->
+<div class="container-xxl py-6" style="padding: 4px">
+    <div class="container">
+        <div class="row g-5">
+            <div class="col-lg-8 wow">
+                <h1 class="display-6 mb-4">FINAL EXAM</h1>
+                <div class="accordion col-lg-10 wow" id="accordionPanelsStayOpenExample">
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="panelsStayOpen-headingOne">
+                            <button class="accordion-button" type="button" data-bs-toggle="collaps" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
+                                Answer Question
+                            </button>
+                        </h2>
+                        <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
+                            <div class="accordion-body">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <p class="card-text">Question &emsp;&emsp;&emsp;&emsp;&nbsp;: <i class="bi bi-book"></i> {{$jumlah_soal}}</p>
+                                        <p class="card-text">Duration &emsp;&emsp;&emsp;&emsp;&nbsp;&nbsp;: <i class="bi bi-clock"></i> {{$exam['durasi']}} Minutes</p>
+                                        {{-- <p class="card-text">TIMER &emsp;&emsp;&emsp;&emsp;&emsp;&nbsp;&nbsp;: <i class="bi bi-clock"></i></p> --}}
+                                        <h1 id="timer"></h1>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <form method="POST" action="/user-submit-exam/{{$exam['id']}}/{{$id_sub_course}}" id="formJE">
+                    @csrf
+                    @php
+                        $no=($soal->currentpage()-1)* $soal->perpage()+1;
+                    @endphp
+                    @foreach ($soal as $index => $s)
+                    <br>
+                    <div class="form-group">
+                      <div class="accordion-item">
+                            <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
+                                <div class="accordion-body">
+                                    <nav aria-label="breadcrumb">
+                                        <ol class="breadcrumb">
+                                          <li class="breadcrumb-item active" aria-current="page">{{$no++}}</li>
+                                        </ol>
+                                    </nav>
+                                    <div class="card">
+                                        <div class="card-body">
+                                            <p class="card-text"> 
+                                                {!! $s['pertanyaan'] !!}
+                                                <input type="hidden" class="form-control" id="exampleFormControlInput1" value="{{$index+1}}" name="id_soal[]">
+                                                <input type="hidden" class="form-control" id="exampleFormControlInput2" value="{{$currentExam['id']}}" name="id_attempt_exam">
+                                                @if($s['audio_file'])
+                                                    <audio controls preload="none">
+                                                        <source src="{{ asset('audio-soal/' . $s['audio_file'] . '/' . $s['audio_file'])}}" type="audio/{{ pathinfo($s['audio_file'], PATHINFO_EXTENSION) }}">
+                                                        Your browser does not support the audio element.
+                                                    </audio>
+                                                @endif
+                                            </p>
+                                            @if($s['tipe'] === 'opsi')
+                                                @foreach ($s->opsi as $op)
+                                                    <p class="card-text">
+                                                        <div class="form-check">
+                                                            <input class="form-check-input select_ans_exam" type="radio" name="user_answers[{{$s['id']}}][{{$index+1}}]" id="exampleRadios{{$s['id']}}_{{$index+1}}" value="{{$op['id']}}" @if(in_array($op['id'], $user_answers)) checked @endif>
+                                                            <label class="form-check-label" for="exampleRadios{{$index+1}}">
+                                                                {!! $op['opsi'] !!}
+                                                                @if($op['audio_file'])
+                                                                    <audio controls preload="none">
+                                                                        <source src="{{ asset('audio-opsi/' . $op['audio_file'] . '/' . $op['audio_file'])}}" type="audio/{{ pathinfo($op['audio_file'], PATHINFO_EXTENSION) }}">
+                                                                        Your browser does not support the audio element.
+                                                                    </audio>
+                                                                @endif
+                                                            </label>
+                                                        </div>
+                                                    </p>
+                                                @endforeach
+                                            @else
+                                            @php
+                                                // Find the user answer for this question
+                                                $userAnswerDesc = $user_answers_desc->where('id_question', $s['id'])->first();
+                                            @endphp
+                                            <div class="input-group">
+                                                <textarea rows="7" class="form-control answer_description_exam" name="user_answers[{{$s['id']}}][{{$index+1}}]" aria-label="With textarea">{{$userAnswerDesc ? $userAnswerDesc['user_answer'] : ''}} </textarea>
+                                            </div>
+                                            @endif
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    @endforeach
+                    <br>
+                    @if($soal->onLastPage())
+                        <button type="submit" class="btn btn-primary submitExam">Submit Tryout</button>
+                    @endif
+                </form>
+                <br>
+                {{ $soal->links() }}
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Courses End -->
+
+<script>
+    $(document).ready(function() {
+        var csrfToken = '{{ csrf_token() }}';
+        $('.select_ans_exam').change(function() {
+            var formData = $('#formJE').serialize();
+            $.ajax({
+                type: 'POST',
+                url: '{{ route("save-answer-exam") }}',
+                data: formData,
+                headers: {
+                    'X-CSRF-TOKEN': csrfToken
+                },
+                success: function(response) {
+                    // console.log(response);
+                },
+                error: function(xhr, status, error) {
+                    // console.error(xhr.responseText);
+                }
+            });
+        });
+    });
+</script>
+<script>
+    $(document).ready(function() {
+        var csrfToken = '{{ csrf_token() }}';
+        $('.answer_description_exam').keyup(function() {
+            var formData = $('#formJE').serialize();
+            $.ajax({
+                type: 'POST',
+                url: '{{ route("save-answer-exam") }}',
+                data: formData,
+                headers: {
+                    'X-CSRF-TOKEN': csrfToken
+                },
+                success: function(response) {
+                    // console.log(response);
+                },
+                error: function(xhr, status, error) {
+                    // console.error(xhr.responseText);
+                }
+            });
+        });
+    });
+</script>
+
+<script>
+    function startTimer(duration) {
+      var timerDisplay = document.getElementById('timer');
+    
+      var startTime = localStorage.getItem('startTimeExam');
+      if (!startTime) {
+        startTime = Date.now();
+        localStorage.setItem('startTimeExam', startTime);
+      }
+    
+      var timer = Math.max(duration * 60 - Math.floor((Date.now() - startTime) / 1000), 0);
+    
+      var interval = setInterval(function () {
+        var hours = Math.floor(timer / 3600);
+        var minutes = Math.floor((timer % 3600) / 60);
+        var seconds = timer % 60;
+    
+        timerDisplay.textContent = hours + "h : " + minutes + "m : " + seconds + "s ";
+    
+        if (--timer < 0) {
+          clearInterval(interval);
+          timerDisplay.textContent = "Time's up!";
+          localStorage.removeItem('startTimeExam');
+          document.getElementById('formJT').submit(); // Submit the form
+        }
+      }, 1000);
+    }
+    
+    // Change the value of minutes here
+    var minutes = {{$exam['durasi']}};
+    startTimer(minutes);
+</script>
+
+<script>
+    $(document).ready(function() {
+        $('.submitExam').on('click',function(e){
+        e.preventDefault();
+        const form = $(this).closest('form');
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "Your Answer Will Submit Immediately!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#09bf25',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, submit it!',
+            width: '600px',
+            height: '20px'
+            }).then((result) => {
+            if (result.value) {
+                localStorage.removeItem('startTimeExam');
+                form.submit();
+            }else{
+                Swal.fire({
+                title: 'Canceled!',
+                icon: 'error',
+                timer: 1300,
+                showConfirmButton: false, 
+            })
+            }
+        })
+        });
+    });
+  </script>
+
+@endsection
